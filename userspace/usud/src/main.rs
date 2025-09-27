125e82ab236f29ecd6c3e378abb68353f0d25a2e
Miguel Vicente <oakymacintosh@duck.com>
2025-09-27 12:12:01 -0300

    add functions to get OS info

--- userspace/usud/src/main.rs
+++ userspace/usud/src/main.rs
@@ -7,6 +7,16 @@ struct UserSu {
     UserShell: String
 }
 
+#[repr(C)]
+struct UtsName {
+    sysname: [libc::c_char; 65],
+    nodename: [libc::c_char; 65],
+    release: [libc::c_char; 65],
+    version: [libc::c_char; 65],
+    machine: [libc::c_char; 65],
+    domainname: [libc::c_char; 65],
+}
+
 pub fn get_os_type() -> &'static str {
     if cfg!(target_os = "linux") {
         "Linux"
@@ -29,4 +39,22 @@ pub fn get_os_type() -> &'static str {
     } else {
         "Non-Unix"
     }
+}
+
+fn get_system_info() -> Result<(), Box<dyn std::error::Error>> {
+    let mut uts: UtsName = unsafe { mem::zeroed() };
+    
+    let result = unsafe { libc::uname(&mut uts as *mut UtsName as *mut libc::utsname) };
+    
+    if result == 0 {
+        let sysname = unsafe { CStr::from_ptr(uts.sysname.as_ptr()) };
+        let release = unsafe { CStr::from_ptr(uts.release.as_ptr()) };
+        let machine = unsafe { CStr::from_ptr(uts.machine.as_ptr()) };
+        
+        println!("System: {}", sysname.to_str()?);
+        println!("Release: {}", release.to_str()?);
+        println!("Machine: {}", machine.to_str()?);
+    }
+    
+    Ok(())
 }
